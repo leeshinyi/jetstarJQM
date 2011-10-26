9cb94cc7bc50b221408b566e0b75de84d9edcb88
russell <mrrussellong@gmail.com>
2011-10-26 14:56:31 +0800

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ JetstarJQM::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( application.js jqueryui.js jquery.iphonepicker.js jQuery.ui.datepicker.js jquery.mobile.datepicker.js )
+  config.assets.precompile += %w( application.js jqueryui.js jquery.iphonepicker.js jQuery.ui.datepicker.js jquery.mobile.datepicker.js jquery.ui.datepicker.css style.css custom.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
